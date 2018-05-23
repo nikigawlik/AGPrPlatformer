ad018a8c6b47411d2a7a254bd5b1d33d40a8ed1f
Nils Gawlik <nilsgawlik@gmx.de>
2018-05-23 18:53:33 +0200

    Implement railing
    This commit apparently contains some meta files that shouldn't be there

--- Assets/Scripts/ColliderToMesh.cs
+++ Assets/Scripts/ColliderToMesh.cs
@@ -1,5 +1,5 @@
 ﻿/// <summary>
-/// Source:
+/// Based on:
 /// http://answers.unity3d.com/questions/835675/how-to-fill-polygon-collider-with-a-solid-color.html
 /// </summary>
 
@@ -8,6 +8,7 @@ using UnityEngine;
 [RequireComponent(typeof(PolygonCollider2D))]
 [RequireComponent(typeof(MeshFilter))]
 [RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(LineRenderer))]
 [ExecuteInEditMode]
 public class ColliderToMesh : MonoBehaviour {
     PolygonCollider2D pc2 ;
@@ -24,6 +25,8 @@ public class ColliderToMesh : MonoBehaviour {
             int pointCount = 0;
             pointCount = pc2.GetTotalPointCount();
             MeshFilter mf = GetComponent<MeshFilter>();
+            LineRenderer lr = GetComponent<LineRenderer>();
+            lr.positionCount = pointCount;// + 1;
             Mesh mesh = new Mesh();
             Vector2[] points = pc2.points;
             Vector3[] vertices = new Vector3[pointCount];
@@ -32,7 +35,10 @@ public class ColliderToMesh : MonoBehaviour {
                 Vector2 actual = points[j];
                 vertices[j] = new Vector3(actual.x, actual.y, 0);
                 uv[j] = actual;
+
+                lr.SetPosition(j, new Vector3(actual.x, actual.y, 0));
             }
+            // lr.SetPosition(pointCount, new Vector3(points[0].x, points[0].y, 0));
             Triangulator tr = new Triangulator(points);
             int [] triangles = tr.Triangulate();
             mesh.vertices = vertices;
